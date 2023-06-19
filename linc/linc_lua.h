f5a32e725a5b722a993831faf7121696ff8a9175
swordcube <49110074+swordcube@users.noreply.github.com>
2023-06-19 00:29:00 -0500

    listen up you shithead!!

--- linc/linc_lua.h
+++ linc/linc_lua.h
@@ -1,7 +1,9 @@
 #pragma once
 
-//hxcpp include should always be first    
+//hxcpp include should always be first 
+#ifndef HXCPP_H
 #include <hxcpp.h>
+#end
 #include <hx/CFFI.h>
 
 //include other library includes as needed
